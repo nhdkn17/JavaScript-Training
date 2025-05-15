b3da1c1d21108555639a8b67a079f7896ad7e230
Nguyenhaidang45 <nguyenhaidang4524@gmail.com>
2025-05-15 22:33:33 +0700

    Section 2 Practice Part 2

---
+++ Section02/script_02.js
@@ -0,0 +1,29 @@
+// condition ? expession1 : expession2;
+
+let isLoggedIn = true;
+isLoggedIn ? console.log("Sign In") : console.log("Sign Out");
+
+// switch (variable) {
+//     case value_1:
+//         // execute if variable === value_1
+//         break;
+//     case value_2:
+//         // ...
+//         break;
+//     default:
+//         // execute if no case matches
+// }
+
+let day = 3;
+switch (day) {
+    case 1:
+        console.log("Thứ hai"); break;
+    case 2:
+        console.log("Thứ ba"); break;
+    case 3:
+        console.log("Thứ tư"); break;
+    default:
+        console.log("Không xác định");
+}
+
+
