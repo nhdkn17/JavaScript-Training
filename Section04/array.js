e4cac1567e992e678bce025e08136f94629c3007
Nguyenhaidang45 <nguyenhaidang4524@gmail.com>
2025-05-21 22:08:26 +0700

    Section 4 Practice

---
+++ Section04/array.js
@@ -0,0 +1,27 @@
+// let numbers = [1, 2, 3, 4, 5];
+// let colors = new Array("Đỏ", "Xanh", "Vàng");
+
+// tenMang[index];
+
+let animals = ["Mèo", "Chó", "Cá"];
+console.log(animals[0]);
+console.log(animals[2]);
+
+let numbers = [1, 2, 3];
+numbers[1] = 10;
+console.log(numbers); // [1, 10, 3]
+
+let mixed = [1, "JS", true, [4, 5], {name: "Lan"}];
+
+mixed.indexOf("JS");
+mixed.includes("JS");
+Array.isArray([1, 2, 3]);
+
+// array.fill(value, start, end);
+let arr = [1, 2, 3, 4, 5];
+arr.fill(9, 1, 4);
+console.log(arr); // [1, 9, 9, 9, 5]
+
+let names = ["An", "Lan", "Ngọc", "Minh"];
+let ketQua = names.filter(name => name.length > 3);
+console.log(ketQua);
